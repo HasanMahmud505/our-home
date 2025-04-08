3c63a7ed065fea1f2d6446b68a1293381dace24f
Hasan <mahasanunigroup@gmail.com>
2025-04-09 01:07:43 +0600

    Hero Slider Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
-hamburger = document.querySelector('.hamburger');
+$(document).ready(function(){
+
+    /**
+     * Hamburger starts  
+     */
+    hamburger = document.querySelector('.hamburger');
 nav = document.querySelector('nav');
 
 hamburger.onclick = function(){
     nav.classList.toggle('active')
 }
+/**
+ * Hamburger ends
+ */
+
+$('.banner-slider').slick({
+    dots: true,
+    infinite: true,
+    arrow:true,
+    speed: 300,
+    slidesToShow: 1,
+    drag:true,
+    adaptiveHeight: true,
+    prevArrow: '<div class="slick-arrow prev"><span><i class="fa-solid fa-arrow-left"></i></span></div>',
+    nextArrow: '<div class="slick-arrow next"><span><i class="fa-solid fa-arrow-right"></i></span></div>',
+});
+
+})
