277ecac19fe36452e2d6e20403e9a70576778669
Hasan <mahasanunigroup@gmail.com>
2025-04-16 00:53:17 +0600

    web site done

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,4 +25,59 @@ $('.banner-slider').slick({
     nextArrow: '<div class="slick-arrow next"><span><i class="fa-solid fa-arrow-right"></i></span></div>',
 });
 
+/**
+ * about section start
+ */
+
+$('.about-us').magnificPopup({ 
+    type: "iframe",
+    iframe: {
+        markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+      
+        patterns: {
+          youtube: {
+            index: 'youtube.com/',       
+            id: 'v=',
+      
+            src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+          },
+      
+        },
+      
+        srcAction: 'iframe_src',
+      }})
+
+/**
+ * Gallery
+ */
+
+$(".gl-image-link").magnificPopup({
+    type: 'image',
+    gallery : {
+        enabled : true
+    }
+})
+
+
+/**
+ * 
+ * scroll to top
+ */
+
+$(window).scroll(function(){
+  if($(this).scrollTop()>100){
+    $('#scrollToTop').fadeIn();
+  }else{
+    $('#scrollToTop').fadeOut();
+  }
+});
+$('#scrollToTop').click(function(){
+  $('html, body').animate({
+    scrollTop:0
+  }, 2000);
+})
+
 })
